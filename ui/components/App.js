c045a2935aef4218c4595bdf90b678ce6fdbf656
aligos <artasikindonesia@gmail.com>
2016-08-04 19:15:18 +0700

    make it better 3

--- ui/components/App.js
+++ ui/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 
 class App extends Component {
   render() {
 
     return (
       <div>
+        <Helmet title="Quran Hub Clone" />
         {this.props.children}
       </div>
     );
